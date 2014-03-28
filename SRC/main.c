be8579637f2eb3456fdac3e8ae0bb6c14f345e82
REYNAUD Nicolas <rn04328l@cispc.c2m.univ-st-etienne.fr>
2014-03-28 17:01:31 +0100

    Les arbres c'est cool et les barrieres aussi

--- SRC/main.c
+++ SRC/main.c
@@ -24,41 +24,35 @@ float theta = 90., phi = 0.;
 void affiche_cube(int x1, int y1, int z1, int x2, int y2, int z2 , GLenum mode) {
 
 	glBegin( mode );
-	/* face avant */
+
 	glVertex3f(x1, y1, z1);
-	glVertex3f(x1, y2, z1);
-	glVertex3f(x1, y2, z2);
 	glVertex3f(x1, y1, z2);
+	glVertex3f(x1, y2, z2);
+	glVertex3f(x1, y2, z1);
 	
 	/*face droite*/	
-	glVertex3f(x1, y2, z2);
+	glVertex3f(x1, y1, z1);
 	glVertex3f(x2, y2, z2);
-	glVertex3f(x2, y2, z1);
-	glVertex3f(x1, y2, z1);
+	glVertex3f(x1, y2, z2);
+	glVertex3f(x2, y1, z1);
 	
 	/*face bas*/
-	glVertex3f(x1, y1, z1);
-	glVertex3f(x2, y1, z1);
+	glVertex3f(x2, y1, z2);
+	glVertex3f(x2, y2, z2);
 	glVertex3f(x2, y2, z1);
 	glVertex3f(x1, y2, z1);
 	
 	/*face arrière*/
-	glVertex3f(x2, y1, z2);
-	glVertex3f(x2, y2, z2);
 	glVertex3f(x2, y2, z1);
-	glVertex3f(x2, y1, z1);
-	
-	/*face gauche*/
-	glVertex3f(x2, y1, z2);
 	glVertex3f(x2, y1, z1);
 	glVertex3f(x1, y1, z1);
 	glVertex3f(x1, y1, z2);
-	
-	/*face du haut*/
 	glVertex3f(x2, y1, z2);
-	glVertex3f(x2, y2, z2);
-	glVertex3f(x1, y2, z2);
+
+	glVertex3f(x1, y2, z1);
+	glVertex3f(x2, y2, z1);	
 	glVertex3f(x1, y1, z2);
+
 	glEnd();
 }
 
@@ -73,47 +67,43 @@ void change_vue() {
 /* Gestion du clavier */
 void gererClavier(unsigned char touche, int x , int y) {
 	int modifiers = glutGetModifiers();
-	float speed = 1;
+	Point svlookat = lookat;
+	float speed = 1.5;
+
     	if (modifiers == GLUT_ACTIVE_SHIFT || modifiers == GLUT_ACTIVE_ALT)
-		speed = 2.1337;	
+		speed = 3.1337;	
 
 	if ( touche == 's' || touche == 'S') {
-		lookat.x -= speed * vue_plane.x;
-		lookat.y -= speed * vue_plane.y;
+		svlookat.x -= speed * vue_plane.x;
+		svlookat.y -= speed * vue_plane.y;
 	}
 	else if ( touche == 'z' || touche == 'Z') {
-		lookat.x += speed * vue_plane.x;
-		lookat.y += speed * vue_plane.y;
+		svlookat.x += speed * vue_plane.x;
+		svlookat.y += speed * vue_plane.y;
 	}
 	else if ( touche == 'q' || touche == 'Q') { 
-		lookat.x += -(speed * vue_plane.y);
-		lookat.y += speed * vue_plane.x;
+		svlookat.x += -(speed * vue_plane.y);
+		svlookat.y += speed * vue_plane.x;
 	}	
 	else if ( touche == 'd' || touche == 'D') {
-		lookat.x += speed * vue_plane.y;
-		lookat.y += -(speed * vue_plane.x);
+		svlookat.x += speed * vue_plane.y;
+		svlookat.y += -(speed * vue_plane.x);
 	}
 	else if ( touche == '2') {
-		lookat.z--;
+		svlookat.z--;
 	}
 	else if ( touche == '8' ) {
-		lookat.z++;
+		svlookat.z++;
 	}
 	else if ( (int)touche == 27) /* METHODE DE GROS PATARASSE A CHANGER */
 		exit(EXIT_FAILURE);
-
-	printf("Touche : %c - Souris : %d %d \n", touche, x , y);
+	
+	if ( svlookat.x > 2 && svlookat.y > 2 && svlookat.x < 1248 && svlookat.y < 1248 && svlookat.z > 5) {
+		lookat = svlookat;
+	} 
+	
+	
 	change_vue();
-}
-
-void afficher_surface(int x, int y, GLenum mode) {
-	glBegin(mode);
-	glVertex3f(x, x, 0);
-	glVertex3f(x, y, 0);
-	glVertex3f(y, y, 0);
-	glVertex3f(y, x, 0);
-	glEnd();
-
 }
 
 void vMotion(int x, int z) {
@@ -145,40 +135,159 @@ void vMotion(int x, int z) {
 	return;
 }
 
+void tree(int x, int y, int z) {
+	/* Sapin les murs */
+	glColor3f(0, 1, 0);
+	glPushMatrix(); 
+		glTranslatef(x, y, z); 
+		glutWireCone(10., 30., 10, 10);
+		//glutSolidCone(10., 25., 10, 10);
+		//glutWireSphere(10., 10, 10);
+	glPopMatrix();
+
+	/* TRONC Arbre */
+	glColor3f(0.95, 0.7, 0.05);
+	glPushMatrix();
+
+		glTranslatef(x, y, 0);
+		glBegin(GL_LINE_LOOP);
+		glVertex3f(-2, 0, 0);
+		glVertex3f(2, 0, 0);
+		glVertex3f(2, 0, 10);
+		glVertex3f(-2, 0, 10);
+		glEnd();
+
+		/* Ligne milieu TRONC */
+		glBegin(GL_LINE_STRIP);
+		glVertex3f(0, 0, 0);
+		glVertex3f(0, 0, 10);
+		glEnd();
+
+		/* Génération Arbre TRONC */
+		glBegin(GL_LINE_LOOP);
+		glVertex3f(0, -2, 0);
+		glVertex3f(0, 2, 0);
+		glVertex3f(0, 2, 10);
+		glVertex3f(0, -2, 10); 		
+		glEnd();
+		
+	glPopMatrix();
+}
+
 void Affichage() {
 	int i, j;
 
 	glMatrixMode(GL_PROJECTION);
 	glLoadIdentity();
-
-	glFrustum(-5, 5, -5, 10, 5, 100);
+	gluPerspective(70, (double)WIDTH/HEIGHT, 1, 600);
+//	glFrustum(-5, 5, -5, 5, 5, 500);
 	gluLookAt(lookat.x, lookat.y, lookat.z, vue.x, vue.y , vue.z, 0, 0, 1);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
 	//glColor3f(0, 0, 0);
 	//afficher_surface(-50, 50, GL_QUADS);
-
-	glColor3f(0, 1, 0);
-	glPushMatrix(); 
-	glTranslatef(0,0,10); 
-	glutWireCone(10., 25., 10, 10);
-	//glutSolidCone(10., 25., 10, 10);
-	//glutSolidSphere(10., 10, 10);
-	//glutWireSphere(15., 10, 10);
-	glPopMatrix();
-
-
-	glColor3f(0.4, 0.4, 0.2);
-	glPushMatrix();
-	affiche_cube(-2, -2, 0, 2, 2, 10, GL_QUADS);
 	
-
+	tree(-30, 10, 10);
+	tree(-50, 40, 10);
+	tree(-50, 20, 10);
+	tree(15, -100, 10);
+	tree(20, 20, 50);
+	/* LE SOLEILLLLLLL */
+	glPushMatrix();
+		glTranslatef(0,150,300); 
+		glColor3f(1, 1, 0);
+		glutWireSphere(20., 15, 15);
 	glPopMatrix();
 
+	/* 1250 : LARGEUR laby = HAUTEUR laby ici*/ 
+	/* Génération du sol */
 	glColor3f(0, 1, 1);
-	for ( i = -50; i < 50; i+= 10 )
-		for ( j = -50; j < 50; j+= 10)
-			afficher_surface(i, j + 10, GL_LINE_LOOP);
+	for ( i = 0; i < 1250; i += 25 ) {
+		for ( j = 0; j < 1250; j += 25) { 
+			glBegin(GL_LINE_LOOP);
+			glVertex3f(i, j, 0);
+			glVertex3f(i + 25, j, 0);
+			glVertex3f(i + 25, j + 25, 0); 
+			glVertex3f(i, j + 25, 0);
+			glEnd();
+		}
+	}
+	
+	/* Mur confinement */
+	glColor4f(1, 0, 0,0.1);
+	glBegin(GL_QUADS);
+	glVertex3f(0, 0, 5);
+	glVertex3f(0, 1250, 5);
+	glVertex3f(0, 1250, 10);
+	glVertex3f(0, 0, 10);
+
+	glVertex3f(0, 1250, 5);
+	glVertex3f(1250, 1250, 5);
+	glVertex3f(1250, 1250, 10);
+	glVertex3f(0, 1250, 10);
+
+	glVertex3f(1250, 1250, 5);
+	glVertex3f(1250, 0, 5);
+	glVertex3f(1250, 0, 10);
+	glVertex3f(1250, 1250, 10);
+
+	glVertex3f(1250, 0, 5);
+	glVertex3f(0, 0, 5);
+	glVertex3f(0, 0, 10);
+	glVertex3f(1250, 0, 10);
+	glEnd();
+
+	glColor3f(1, 0, 0);
+
+	/* boiboite */
+	glBegin(GL_QUADS);
+	/* Boiboite de la face gauche au spawn */
+	glVertex3f(0, 0, 4.5);
+	glVertex3f(0, 1250, 4.5);
+	glVertex3f(0, 1250, 5);
+	glVertex3f(0, 0, 5);
+
+	glVertex3f(0, 0, 10);
+	glVertex3f(0, 1250, 10);
+	glVertex3f(0, 1250, 10.5);
+	glVertex3f(0, 0, 10.5);
+
+	/* Autre boibote */
+	glVertex3f(0, 1250, 4.5);
+	glVertex3f(1250, 1250, 4.5);
+	glVertex3f(1250, 1250, 5);
+	glVertex3f(0, 1250, 5);
+
+	glVertex3f(0, 1250, 10);
+	glVertex3f(1250, 1250, 10);
+	glVertex3f(1250, 1250, 10.5);
+	glVertex3f(0, 1250, 10.5);
+
+	/* autre boiboite */
+	glVertex3f(1250, 1250, 4.5);
+	glVertex3f(1250, 0, 4.5);
+	glVertex3f(1250, 0, 5);
+	glVertex3f(1250, 1250, 5);
+
+	glVertex3f(1250, 1250, 10);
+	glVertex3f(1250, 0, 10);
+	glVertex3f(1250, 0, 10.5);
+	glVertex3f(1250, 1250, 10.5);
+
+	/* autre boiboite */
+	glVertex3f(1250, 0, 4.5);
+	glVertex3f(0, 0, 4.5);
+	glVertex3f(0, 0, 5);
+	glVertex3f(1250, 0, 5);
+
+	glVertex3f(1250, 0, 10);
+	glVertex3f(0, 0, 10);
+	glVertex3f(0, 0, 10.5);
+	glVertex3f(1250, 0, 10.5);
+
+
+	glEnd();
+
 
 	glutWarpPointer(MID_WIDTH, MID_HEIGHT);
 	
@@ -187,7 +296,7 @@ void Affichage() {
 }
 
 int main ( int argc, char * argv[] ) {
-	float xlookat = 5, ylookat = -55, zlookat = 10;
+	float xlookat = 12.5, ylookat = 12.5, zlookat = 10;
 
 	vec = kCreePoint(0, EPSILON, 0);
 	vue_plane = kCreePoint(0, 1, 0);
@@ -201,6 +310,9 @@ int main ( int argc, char * argv[] ) {
 	glutInitWindowPosition(300, 50);
 
 	glutCreateWindow("Laby");
+	glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+	glEnable( GL_BLEND );
+
 	glEnable(GL_DEPTH_TEST);
 	glutSetCursor(GLUT_CURSOR_NONE);
 	glutWarpPointer(MID_HEIGHT , MID_HEIGHT);
