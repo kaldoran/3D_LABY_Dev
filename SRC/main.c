04d485b83e024e21987823bb23edebc3057207ee
REYNAUD Nicolas <rn04328l@cispc.c2m.univ-st-etienne.fr>
2014-03-28 12:48:16 +0100

    Arbres

--- SRC/main.c
+++ SRC/main.c
@@ -4,51 +4,57 @@
 #include <stdlib.h>
 #include <limits.h>
 #include <math.h>
-
+#include <float.h>
 #include "karbre.h"
 
 #define HEIGHT 800
 #define MID_HEIGHT HEIGHT / 2
-#define WIDTH 700
+#define WIDTH 800
 #define MID_WIDTH WIDTH / 2
 
 #define EPSILON 20
-#define SENSIBILITE 0.2
+#define SENSIBILITE 0.07
 
-Point vue, lookat, vec;
+
+Point vue, lookat, vec, vue_plane;
 float theta = 90., phi = 0.;
 
 /* USELESS */
 
 void affiche_cube(int x1, int y1, int z1, int x2, int y2, int z2 , GLenum mode) {
 
-	glColor3f(0, 1, 0);
 	glBegin( mode );
+	/* face avant */
 	glVertex3f(x1, y1, z1);
 	glVertex3f(x1, y2, z1);
 	glVertex3f(x1, y2, z2);
 	glVertex3f(x1, y1, z2);
 	
+	/*face droite*/	
 	glVertex3f(x1, y2, z2);
 	glVertex3f(x2, y2, z2);
 	glVertex3f(x2, y2, z1);
 	glVertex3f(x1, y2, z1);
 	
+	/*face bas*/
 	glVertex3f(x1, y1, z1);
 	glVertex3f(x2, y1, z1);
 	glVertex3f(x2, y2, z1);
 	glVertex3f(x1, y2, z1);
 	
+	/*face arrière*/
 	glVertex3f(x2, y1, z2);
 	glVertex3f(x2, y2, z2);
 	glVertex3f(x2, y2, z1);
 	glVertex3f(x2, y1, z1);
 	
+	/*face gauche*/
 	glVertex3f(x2, y1, z2);
 	glVertex3f(x2, y1, z1);
 	glVertex3f(x1, y1, z1);
 	glVertex3f(x1, y1, z2);
 	
+	/*face du haut*/
 	glVertex3f(x2, y1, z2);
 	glVertex3f(x2, y2, z2);
 	glVertex3f(x1, y2, z2);
@@ -62,24 +68,30 @@ void change_vue() {
 	vue.z = lookat.z + vec.z;
 }
 
+
+
 /* Gestion du clavier */
 void gererClavier(unsigned char touche, int x , int y) {
-	
-	if ( touche == 's') {
-		lookat.x -= vec.x;
-		lookat.y -= vec.y;
+	int modifiers = glutGetModifiers();
+	float speed = 1;
+    	if (modifiers == GLUT_ACTIVE_SHIFT || modifiers == GLUT_ACTIVE_ALT)
+		speed = 2.1337;	
+
+	if ( touche == 's' || touche == 'S') {
+		lookat.x -= speed * vue_plane.x;
+		lookat.y -= speed * vue_plane.y;
 	}
-	else if ( touche == 'z') {
-		lookat.x += vec.x;
-		lookat.y += vec.y;
+	else if ( touche == 'z' || touche == 'Z') {
+		lookat.x += speed * vue_plane.x;
+		lookat.y += speed * vue_plane.y;
 	}
-	else if ( touche == 'd') { 
-		lookat.x++;
-		vue.x++;
+	else if ( touche == 'q' || touche == 'Q') { 
+		lookat.x += -(speed * vue_plane.y);
+		lookat.y += speed * vue_plane.x;
 	}	
-	else if ( touche == 'q') {
-		lookat.y--;
-		vue.x--;
+	else if ( touche == 'd' || touche == 'D') {
+		lookat.x += speed * vue_plane.y;
+		lookat.y += -(speed * vue_plane.x);
 	}
 	else if ( touche == '2') {
 		lookat.z--;
@@ -93,7 +105,8 @@ void gererClavier(unsigned char touche, int x , int y) {
 	printf("Touche : %c - Souris : %d %d \n", touche, x , y);
 	change_vue();
 }
-void afficher_surface(int x, int y, GLenum  mode) {
+
+void afficher_surface(int x, int y, GLenum mode) {
 	glBegin(mode);
 	glVertex3f(x, x, 0);
 	glVertex3f(x, y, 0);
@@ -118,41 +131,17 @@ void vMotion(int x, int z) {
 	else if ( phi < -89)
 		phi = -89;
 
-	printf("theta : %f - phi : %f, vue.x : %f - vue.y : %f\n", theta, phi, vue.x, vue.y);	
-	//theta = (double)(deltaX * 90) / MID_WIDTH;
+	//printf("theta : %f - phi : %f, vue.x : %f - vue.y : %f\n", theta, phi, vue.x, vue.y);	
 	tmp = cos(phi * M_PI / 180);
 
-	vec.x = tmp * cos(theta * M_PI / 180);
-	vec.y = tmp * sin(theta * M_PI / 180);
+	vue_plane.x = cos(theta * M_PI / 180);
+	vue_plane.y = sin(theta * M_PI / 180);
+
+	vec.x = tmp * vue_plane.x;
+	vec.y = tmp * vue_plane.y;
 	vec.z = sin(phi * M_PI / 180);
-
+	
 	change_vue();
-	/*
-	float centerX, centerY, deltaX, deltaZ, sin_angle, cos_angle;
-	centerX  = largeur_rep / 2;
-	centerY = hauteur_rep / 2;
-	deltaX = (x - centerX) / SENSIBILITE;
-	deltaZ = y - centerY;
-	
-	float angleX;
-
-	angleX = atan(deltaX / EPSILON);
-	
-
-	angle += angleX;
-	sin_angle = sin(angle);
-	cos_angle = cos(angle);
-
-	vue.x = xlookat + EPSILON * sin_angle;
-	vue.y = ylookat + EPSILON * cos_angle;  
-	printf("%f %f", sin_angle, cos_angle);
-	if ( sin_angle == 0 && cos_angle == 1 ) 
-		angle = 0;
-
-	printf("Je regarde en %f %f - angle %f - Delta : %f %f \n", vue.x, vue.y, angle , deltaX, deltaZ );
-
-	vue.z += deltaZ / SENSIBILITE;
-	*/
 	return;
 }
 
@@ -166,8 +155,25 @@ void Affichage() {
 	gluLookAt(lookat.x, lookat.y, lookat.z, vue.x, vue.y , vue.z, 0, 0, 1);
 	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
-	glColor3f(0, 0, 0);
-	afficher_surface(-50, 50, GL_QUADS);
+	//glColor3f(0, 0, 0);
+	//afficher_surface(-50, 50, GL_QUADS);
+
+	glColor3f(0, 1, 0);
+	glPushMatrix(); 
+	glTranslatef(0,0,10); 
+	glutWireCone(10., 25., 10, 10);
+	//glutSolidCone(10., 25., 10, 10);
+	//glutSolidSphere(10., 10, 10);
+	//glutWireSphere(15., 10, 10);
+	glPopMatrix();
+
+
+	glColor3f(0.4, 0.4, 0.2);
+	glPushMatrix();
+	affiche_cube(-2, -2, 0, 2, 2, 10, GL_QUADS);
+	
+
+	glPopMatrix();
 
 	glColor3f(0, 1, 1);
 	for ( i = -50; i < 50; i+= 10 )
@@ -183,7 +189,9 @@ void Affichage() {
 int main ( int argc, char * argv[] ) {
 	float xlookat = 5, ylookat = -55, zlookat = 10;
 
-	vue = kCreePoint(xlookat, ylookat + EPSILON, zlookat);
+	vec = kCreePoint(0, EPSILON, 0);
+	vue_plane = kCreePoint(0, 1, 0);
+	vue = kCreePoint(xlookat + vec.x, ylookat + vec.y, zlookat + vec.z);
 	lookat = kCreePoint(xlookat, ylookat, zlookat);
 
 	glutInit(&argc, argv);
@@ -193,6 +201,7 @@ int main ( int argc, char * argv[] ) {
 	glutInitWindowPosition(300, 50);
 
 	glutCreateWindow("Laby");
+	glEnable(GL_DEPTH_TEST);
 	glutSetCursor(GLUT_CURSOR_NONE);
 	glutWarpPointer(MID_HEIGHT , MID_HEIGHT);
 
